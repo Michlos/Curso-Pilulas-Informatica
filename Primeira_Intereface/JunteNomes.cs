e6777ffd58f94af8e16baa891b2cd056c54a2643
MichlosBSB <michlos@gmail.com>
2021-01-24 22:49:55 -0300

    Add project files.

---
+++ Primeira_Intereface/JunteNomes.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Primeira_Intereface
+{
+    class JunteNomes : INotifyPropertyChanged
+    {
+        private string nome, sobreNome, nomeCompleto;
+        public string Nome
+        {
+            get { return nome; }
+            set
+            {
+                nome = value;
+                OnPopertyChanged("NomeCompleto");
+            }
+        }
+        public string SobreNome
+        {
+            get { return sobreNome; }
+            set
+            {
+                sobreNome = value;
+                OnPopertyChanged("NomeCompleto");
+            }
+        }
+        public string NomeCompleto
+        {
+            get
+            {
+                nomeCompleto = Nome + " " + SobreNome;
+                return nomeCompleto;
+            }
+            set
+            {
+                nomeCompleto = value;
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPopertyChanged(string property)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(property));
+            }
+        }
+
+
+    }
+}
---
+++ Primeira_Intereface/MainWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xaml;
+
+namespace Primeira_Intereface
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            SlideGrid.Visibility = Visibility.Hidden;
+            TextBoxesGrid.Visibility = Visibility.Hidden;
+            JuntaNomeESobreNome = new JunteNomes { Nome = "Michael", SobreNome = "Lima" };
+            this.DataContext = JuntaNomeESobreNome;
+        }
+
+        internal JunteNomes JuntaNomeESobreNome;
+
+        private void BtBindingSlide_Click(object sender, RoutedEventArgs e)
+        {
+            if (TextBoxesGrid.Visibility == Visibility.Visible)
+            {
+                TextBoxesGrid.Visibility = Visibility.Hidden;
+                BtBindiningTextBox.Content = "Show TextBoxes";
+            }
+
+            if (SlideGrid.Visibility == Visibility.Hidden)
+            {
+                SlideGrid.Visibility = Visibility.Visible;
+                BtBindingSlide.Content = "Hide Slide";
+            }
+            else
+            {
+                SlideGrid.Visibility = Visibility.Hidden;
+                BtBindingSlide.Content = "Show Slide";
+            }
+        }
+
+        private void BtBindiningTextBox_Click(object sender, RoutedEventArgs e)
+        {
+            if (SlideGrid.Visibility == Visibility.Visible)
+            {
+                SlideGrid.Visibility = Visibility.Hidden;
+                BtBindingSlide.Content = "Show Slide";
+            }
+
+            if (TextBoxesGrid.Visibility == Visibility.Hidden)
+            {
+                TextBoxesGrid.Visibility = Visibility.Visible;
+                BtBindiningTextBox.Content = "Hide TextBoxes";
+            }
+            else
+            {
+                TextBoxesGrid.Visibility = Visibility.Hidden;
+                BtBindiningTextBox.Content = "Show TextBoxes";
+            }
+        }
+    }
+}
